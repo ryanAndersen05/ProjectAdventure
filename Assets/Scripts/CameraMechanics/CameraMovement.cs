2164cf706e70a420db4f3102a0922be6bb22b121
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-06-07 13:30:47 -0400

    CameraMovement added

---
+++ Assets/Scripts/CameraMechanics/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindObjectOfType<PlayerController>().transform;
+    }
+
+    void Update()
+    {
+        updateCameraPosition();
+    }
+
+    void updateCameraPosition()
+    {
+        Vector3 goalPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, goalPosition, .2f);
+        
+
+    }
+
+    public void setTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
